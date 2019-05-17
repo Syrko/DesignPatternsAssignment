61c9b848d59f6310649d78a917a64bca4a23ca1c
Syrko <kostassir@yahoo.gr>
2019-05-17 18:37:25 +0300

    Added LSP Violation and Compliance Projects

---
+++ LSP/LSP_Compliant/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP_Compliant
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Circle C1 = new Circle();
+			Square S1 = new Square();
+
+			Program p = new Program();
+
+			p.DrawShape(C1);
+			p.DrawShape(S1);
+
+			Console.Read();
+		}
+
+		void DrawShape(Shape shape)
+		{
+			shape.Draw();
+		}
+	}
+
+	abstract class Shape
+	{
+		public abstract void Draw();
+	}
+
+	class Circle : Shape
+	{
+		override public void Draw()
+		{
+			Console.WriteLine("A circle");
+		}
+	}
+
+	class Square : Shape
+	{
+		override public void Draw()
+		{
+			Console.WriteLine("A square");
+		}
+	}
+}
+}
---
+++ LSP/LSP_Violation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP_Violation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Circle C1 = new Circle();
+			Square S1 = new Square();
+
+			Program p = new Program();
+
+			p.DrawShape(C1);
+			p.DrawShape(S1);
+
+			Console.Read();
+		}
+
+		void DrawShape(Shape shape)
+		{
+			if(shape.GetType() == typeof(Circle))
+			{
+				((Circle)shape).Draw();
+			}
+			else if (shape.GetType() == typeof(Square))
+			{
+				((Square)shape).Draw();
+			}
+		}
+	}
+
+	abstract class Shape
+	{
+
+	}
+	
+	class Circle: Shape
+	{
+		public void Draw()
+		{
+			Console.WriteLine("A circle");
+		}
+	}
+
+	class Square : Shape
+	{
+		public void Draw()
+		{
+			Console.WriteLine("A square");
+		}
+	}
+}
